bf3cfcfcfa9a18d88bc078b4381ad76c10c73a3b
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-28 18:17:38 -0300

    Refactor CSS and add SinglePokemonCard component

--- src/routes/search.index.tsx
+++ src/routes/search.index.tsx
@@ -3,6 +3,7 @@ import SearchCard from "../components/search-card";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { searchPokemonByName } from "../api/api";
+import SinglePokemonCard from "../components/slngle-pokemon-card";
 
 export const Route = new FileRoute("/search").createRoute({
   component: SearchRoute,
@@ -10,26 +11,31 @@ export const Route = new FileRoute("/search").createRoute({
 
 export default function SearchRoute() {
   const [filter, setFilter] = useState("");
-  // const debouncedFilter = useDebounce(filter, 500);
-  // const { data } = useQuery(
-  //   queryKey: ["searchPokemon"],
-  //   () => searchPokemonByName(filter)
-  //   // { enabled: Boolean(debouncedFilter) }
-  // );
 
-  console.log(filter);
   const { isPending, data } = useQuery({
     queryKey: ["search-pokemon", filter],
     queryFn: () => searchPokemonByName(filter),
-    // { enabled: Boolean(debouncedFilter) }
   });
-  console.log(data);
 
   return (
-    <section className="flex flex-col mx-auto justify-center w-full py-6 sm:py-12 md:py-24">
-      <SearchCard setPokemonSearch={setFilter} />
-
-      {isPending && <div>Loading...</div>}
+    <section className="w-full text-center py-6 sm:py-12 md:py-24">
+      <div className="flex justify-center">
+        <SearchCard setPokemonSearch={setFilter} />
+      </div>
+      {isPending && <div className="mt-10">Loading...</div>}
+      <div className="mt-10">
+        {data?.length === 0 || (data === undefined && !isPending) ? (
+          <h4 className="font-bold text-2xl">Pokemon no found</h4>
+        ) : (
+          <SinglePokemonCard
+            name={data?.name ?? ""}
+            type={data?.types[0].type.name ?? ""}
+            species={data?.species.name ?? ""}
+            weight={data?.weight ?? ""}
+            image={data?.sprites.front_default ?? ""}
+          />
+        )}
+      </div>
     </section>
   );
 }
